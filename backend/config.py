c4869668958ed940d693e19d4f37712e3424be49
Hewerson Freitas <hewerson.freitas@gmail.com>
2020-01-18 16:08:36 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,4 @@ DEBUG = True
 FLASK_DEBUG = True
 FLASK_APP = 'app'
 FLASK_ENV = 'development'
-MONGODB_URI = 'mongodb://api:ZrOadoICMcnAsTsU@cluster0-shard-00-00-wgcjf.mongodb.net:27017,cluster0-shard-00-01-wgcjf.mongodb.net:27017,cluster0-shard-00-02-wgcjf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+MONGODB_URI = ''
